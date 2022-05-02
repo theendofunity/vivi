6df8afc3b7526b3b378ed089f1969f39c080e9e1
d.dudkin <d.dudkin@growapp.biz>
2022-05-02 20:51:51 +0700

    base colors and font

--- Vivi/Extensions/UIKit/UIColor + Extensions.swift
+++ Vivi/Extensions/UIKit/UIColor + Extensions.swift
@@ -15,4 +15,41 @@ extension UIColor {
         b = CGFloat(blue) / 255.0
         self.init(red: r, green: g, blue: b, alpha: alpha)
     }
+    
+    convenience init(hex: Int, a: CGFloat = 1.0) {
+        self.init(
+            red: (hex >> 16) & 0xFF,
+            green: (hex >> 8) & 0xFF,
+            blue: hex & 0xFF,
+            alpha: a
+        )
+    }
+}
+
+//MARK: - Main colors
+
+extension UIColor {
+    var denim: UIColor? {
+        return UIColor(hex: 0x172038)
+    }
+    
+    var viviRose: UIColor? {
+        return UIColor(hex: 0xDCAAAA)
+    }
+    
+    var viviRose50: UIColor? {
+        return viviRose?.withAlphaComponent(0.5)
+    }
+    
+    var viviRose70: UIColor? {
+        return viviRose?.withAlphaComponent(0.7)
+    }
+    
+    var background: UIColor? {
+        return UIColor(hex: 0xF5ECEC)
+    }
+    
+    var viviGreen: UIColor? {
+        return UIColor(hex: 0x4A7574)
+    }
 }
---
+++ Vivi/Extensions/UIKit/UIFont + Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  UIFont + Extensions.swift
+//  Vivi
+//
+//  Created by Дмитрий Дудкин on 02.05.2022.
+//
+
+import UIKit
+
+extension UIFont {
+    var titleFont: UIFont? {
+        return UIFont.systemFont(ofSize: 24)
+    }
+    
+    var subtitleFont: UIFont? {
+        return UIFont.systemFont(ofSize: 24)
+    }
+    
+    var mainFont: UIFont? {
+        return UIFont.systemFont(ofSize: 22)
+    }
+}
